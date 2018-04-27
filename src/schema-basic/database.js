91adfcfcf6985048d8eaf58c621d024526dffa76
sblack4 <genseb7@gmail.com>
2018-04-26 23:27:21 -0400

    it actually works!

--- src/schema-basic/database.js
+++ src/schema-basic/database.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import * as conn from '../connections/connections'
 
 // connect to our database file
 const dataFilePath = path.join(__dirname, '../data/demo-data.sl3')
@@ -6,9 +7,7 @@ const dataFilePath = path.join(__dirname, '../data/demo-data.sl3')
 // knex is a convenient library that can connect to various SQL databases
 // you can use any library you wish
 export default require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: dataFilePath
-  },
+  client: conn.oracle_db.client,
+  connection: conn.oracle_db.connection,
   useNullAsDefault: true
 })
